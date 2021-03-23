2377da76b728ff817add8d92d46a86020ecba808
jasbel <asbel.elmec@gmail.com>
2021-03-23 10:39:20 -0400

    last change

--- src/res/letters.js
+++ src/res/letters.js
@@ -46,7 +46,7 @@ La cruz por mí cargó
 Derramaste allí tu amor
 Mi alma cantará Aleluya
 Gloria y honra a ti Señor`,
-                chorus_position_ignore: [1,3,4]
+                chorus_position_ignore: []
             }
         ],
 
@@ -153,8 +153,8 @@ mas yo pobre pecador.`
 nada bueno hay en mí;
 Solo en ti hay salvación,
 tú das luz al corazón.
-<i>/Ven entonces a mi ser,</i>
-<i>y hazlo tu resplandecer./</i>`
+/Ven entonces a mi ser,
+y hazlo tu resplandecer./`
             }
         ],
         chorus:[],
@@ -208,7 +208,7 @@ Contempla al Rey, nadie es como Él venid y adoradle.`,
         id: "6",
         num_song: "6",
         title_es: "CUAN GRANDE ES DIOS",
-        description_es: " Quién formó las olas al hablar...",
+        description_es: "Cuan esplendor de un rey...",
 
         paragraphs: [
             {
@@ -251,6 +251,122 @@ eres digno de adorar
 siempre cantare
 cuán grande eres Dios.`,
                 chorus_position_ignore: [1]
+            },
+        ],
+    },
+    {
+        id: "7",
+        num_song: "7",
+        title_es: "DESDE QUE TE CONOCÍ",
+        description_es: "Desde que te conocí...",
+
+        paragraphs: [
+            {
+                paragraph:
+`Desde que te conocí, desde que viniste a mi
+Desde que experimente, tu perdón, tu amor, tu paz.
+Algo nuevo en mi empezó por tu espíritu de amor.
+Es tu vida cual semilla que brotó en mi corazón.`
+            },
+            {
+                paragraph:
+`¡Qué momento tan feliz!
+Que alegría recibí;
+al tener en mi vida tu presencia celestial.
+Sé de dónde vengo sé a dónde voy,
+sé quién soy, porque existo y tu propósito en mí.`
+            },
+            {
+                paragraph:
+`Ahora que vives en mí,
+ahora que te puedo oír,
+ahora que crece en mí;
+tu persona sin igual
+Quiero dejar que seas tú para que no sea yo;
+El que manda al que vean pues en mi a ti han de ver`
+            },
+            {
+                paragraph:
+`Correré hasta alcanzar,
+la corona que darás
+En el día que regreses y nos lleves al hogar,
+Allí llanto no habrá, la justicia reinará
+Desde el trono santo y justo reinaras.`
+            },
+        ],
+
+        chorus:[
+            {
+                choir:
+`Quiero amar Oh Cristo,
+quiero dar Oh Cristo,
+quiero ser Oh Cristo como tu
+Al caído levantar al enfermo consolar
+A este mundo que agoniza tu remedio alcanzar`,
+                chorus_position_ignore: [1,3]
+            },
+        ],
+    },
+    {
+        id: "8",
+        num_song: "8",
+        title_es: "DE GLORIA EN GLORIA",
+        description_es: "De gloria en gloria te veo...",
+
+        paragraphs: [
+            {
+                paragraph:
+`/De gloria en gloria te veo,
+Cuanto más te conozco;
+quiero saber más de ti.
+Mi Dios cuán buen alfarero,
+Levántame y transfórmame;
+Moldéame a tu imagen Señor./`
+            },
+        ],
+
+        chorus:[
+            {
+                choir:
+`//Quiero ser más como tú,
+ver la vida como tú
+Satúrame de tu espíritu
+Y reflejar al mundo tu amor. //`,
+                chorus_position_ignore: []
+            },
+        ],
+    },
+    {
+        id: "9",
+        num_song: "9",
+        title_es: "DE LA MANO SOBERANA",
+        description_es: "Cuan feroz llega la prueba...",
+
+        paragraphs: [
+            {
+                paragraph:
+`Cuan feroz llega la prueba acechando el corazón
+Amenaza con sus dudas, destruir mi convicción
+Cuando todo es incierto; cuando es fácil tropezar
+El refugio de mi alma, sé que no se moverá.`
+            },
+            {
+                paragraph:
+`Oh cuan altos Sus caminos; cuan supremo es Su plan
+Todo lo ha hecho hermoso en Su tiempo y voluntad
+Aunque frágiles mis pasos; limitado mi entender
+Su palabra me asegura: todo obra para bien`
+            },
+        ],
+
+        chorus:[
+            {
+                choir:
+`//Quiero ser más como tú,
+ver la vida como tú
+Satúrame de tu espíritu
+Y reflejar al mundo tu amor. //`,
+                chorus_position_ignore: []
             },
         ],
     },
