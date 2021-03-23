39f8f8530b361b9f97219239dda9648c7a6d3338
jasbel <asbel.elmec@gmail.com>
2021-03-23 08:47:53 -0400

    change letters

--- src/res/letters.js
+++ src/res/letters.js
@@ -1,61 +1,257 @@
 export const songs = [
+    
     {
-        id: "1",
-        num_song: "123",
-        title_es: "Himno",
-        description_es: "Esto es un himno",
-        title_qc: "Himno Quechua",
+        id: "2",
+        num_song: "2",
+        title_es: "AQUELLA CRUZ",
+        description_es: "El cordero que sufrió...   ",
 
-        parrafos: [
+        paragraphs: [
             {
-                parrafo: " Una y otra vez \n Al estar yo junto a ti \n No me puedo contener \n Cuando me miras así \n Ya no hay nada que decir \n Eres todo para mi "
+                paragraph:
+`El cordero que sufrió
+Traicionado fue
+El peso de nuestra maldad
+Fue puesto sobre Él`
             },
             {
-                parrafo:
-`Llevarme allá,
-donde sé que habrá paz
-Donde tengo que callar
-Para escucharte hablar
-Donde todo es realidad
-Y el tiempo no existe más`
+                paragraph:
+`En silencio soportó
+Dolor y humillación
+Obediente al Padre fue
+La cruz por mí cargó
+`
+            },
+            {
+                paragraph:
+`Unigénito de Dios
+Viniste a redimir
+Y perdonar a todo aquel
+Que se acerca a ti
+`
+            },
+            {
+                paragraph:
+`En silencio soportó
+Dolor y humillación
+Obediente al Padre fue
+La cruz por mí cargó
+`
             }
         ],
-        coro:
-`Jesús mi fiel amigo
-Mi dulce caminar
-Quédate conmigo
-No quiero volver atrás
-No quiero volver atrás`,
+        chorus:[
+            {
+                choir:
+`En aquella cruz me salvaste
+Derramaste allí tu amor
+Mi alma cantará Aleluya
+Gloria y honra a ti Señor`,
+                chorus_position_ignore: [1,3,4]
+            }
+        ],
 
 
     },
     {
-        id: "2",
-        num_song: "12",
-        title_es: "Himno",
-        description_es: "Esto es un himno",
-        title_qc: "Himno Quechua",
+        id: "3",
+        num_song: "3",
+        title_es: "AQUELLA CRUZ",
+        description_es: "El cordero que sufrió...   ",
 
-        parrafos: [
+        paragraphs: [
             {
-                parrafo: `Una y otra vez 
-                Al estar yo junto a ti 
-                No me puedo contener 
-                Cuando me miras así 
-                Ya no hay nada que decir 
-                Eres todo para mi`
+                paragraph:
+`Ante el trono celestial
+Él intercede hoy por mí
+Gran Sacerdote es Jesús
+Quien por siempre vivirá
+`
             },
             {
-                parrafo: `Llevarme allá, donde sé que habrá paz 
-                Donde tengo que callar 
-                Para escucharte hablar 
-                Donde todo es realidad 
-                Y el tiempo no existe más`
+                paragraph:
+`Cuando he caído en tentación
+Y al sentir condenación
+Al ver al cielo encontraré
+Al inocente quien murió.`
+            },
+            {
+                paragraph:
+`Ante el Cordero Redentor
+Quién al morir resucitó
+El inmutable gran Yo Soy
+El Rey de gloria y majestad`
+            },
+            {
+                paragraph:
+`En silencio soportó
+Dolor y humillación
+Obediente al Padre fue
+La cruz por mí cargó
+`
+            }
+        ],
+        chorus:[
+            {
+                choir:
+`Y en Sus manos por Su amor
+Mi nombre ya grabado está
+Y mientras en el cielo esté
+Nadie de Él me apartará
+Nadie de Él me apartará`,
+                chorus_position_ignore: [2,3]
+            },
+            {
+                choir:
+`Y por su muerte el Salvador
+Ya mi pecado perdonó
+Pues Dios, el Justo, aceptó
+Su sacrificio hecho por mí
+Su sacrificio hecho por mí`,
+                chorus_position_ignore: [1,3]
+            },
+            {
+                choir:
+`//Unido a Él no moriré
+Pues con Su sangre me compró
+Mi vida escondida está
+En Cristo Dios, mi Salvador
+En Cristo Dios, mi Salvador//`,
+                chorus_position_ignore: [1,2]
+            },
+        ],
+
+
+    },
+    {
+        id: "4",
+        num_song: "4",
+        title_es: "COMPADECETE DE MÍ",
+        description_es: "Compadécete de mí...",
+
+        paragraphs: [
+            {
+                paragraph:
+`Compadécete de mí,
+pues oh Dios yo clamo a ti.
+Y con noble compasión,
+borra tú mi rebelión.
+Y hazme puro en verdad,
+redimiendo mi maldad.`
+            },
+            {
+                paragraph:
+`He pecado contra ti,
+y dolor yo siento en mí.
+Pues tu gracia desprecié
+y tus leyes quebranté;
+Tú eres justo oh Señor,
+mas yo pobre pecador.`
+            },
+            {
+                paragraph:
+`En pecado yo nací,
+nada bueno hay en mí;
+Solo en ti hay salvación,
+tú das luz al corazón.
+<i>/Ven entonces a mi ser,</i>
+<i>y hazlo tu resplandecer./</i>`
+            }
+        ],
+        chorus:[],
+    },
+    {
+        id: "5",
+        num_song: "5",
+        title_es: "CONTEMPLA A DIOS",
+        description_es: " Quién formó las olas al hablar...",
+
+        paragraphs: [
+            {
+                paragraph:
+`¿Quién formó las olas al hablar?,
+¿Quién contó la arena en el mar?
+Reyes, pueblos, tiemblan a su voz,
+la creación le rinde adoración.`
+            },
+            {
+                paragraph:
+`¿Quién le dio consejo al Señor?,
+¿Quién cuestiona lo que Él habló?
+Quién al sabio Dios puede enseñar,
+¿Quién comprende su gran majestad?`
+            },
+            {
+                paragraph:
+`¿Quién sufrió los clavos en dolor?
+¿Quién llevo el mal del pecador?
+Jesucristo, humilde al perecer,
+Ha triunfado y reina con poder.`
             }
         ],
 
-        coro: "Jesús mi fiel amigo \n Mi dulce caminar \n Quédate conmigo \n No quiero volver atrás \n No quiero volver atrás"
+        chorus:[
+            {
+                choir:
+`Contempla a Dios, en su trono está venid y adoradle.
+Contempla al Rey, nadie es como Él venid y adoradle.`,
+                chorus_position_ignore: [2,3]
+            },
+            {
+                choir:
+`//¡Reinaras por siempre!//
+//¡Que tu gloria brille hoy!//`,
+                chorus_position_ignore: [1,2]
+            },
+        ],
+    },
+    {
+        id: "6",
+        num_song: "6",
+        title_es: "CUAN GRANDE ES DIOS",
+        description_es: " Quién formó las olas al hablar...",
 
-    }
+        paragraphs: [
+            {
+                paragraph:
+`Cuan esplendor de un rey
+vestido de majestad la tierra adorará, la tierra adorará.
+Rodeado por su luz. tinieblas oscuridad tiembla con su voz.
+Tiembla con su voz.`
+            },
+            {
+                paragraph:
+`Perfecto solo es Él,
+el tiempo suyo es
+principio y final, principio y final.
+Hermosa trinidad
+su esencia en tres está.
+Cordero vive hoy, cordero vive hoy.`
+            },
+        ],
 
+        chorus:[
+            {
+                choir:
+`Cuán grande es Dios, sin igual,
+cuán grande eres Dios,
+todos cantarán
+cuán grande eres Dios.`,
+                chorus_position_ignore: [2]
+            },
+            {
+                choir:
+`//Cuán grande es Dios, sin igual,
+Cuán grande eres Dios, todos cantarán cuán grande eres Dios.//`,
+                chorus_position_ignore: [1]
+            },
+            {
+                choir:
+`Nombre sin igual
+eres digno de adorar
+siempre cantare
+cuán grande eres Dios.`,
+                chorus_position_ignore: [1]
+            },
+        ],
+    },
 ]
