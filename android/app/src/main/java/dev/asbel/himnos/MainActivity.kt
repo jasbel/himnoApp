5101c97b3a6117459084f372f57290825bc0c205
Asbel Jhonatan Apaza Romero <asbel.elmec@gmail.com>
2024-08-13 14:31:36 -0400

    changes

--- android/app/src/main/java/dev/asbel/himnos/MainActivity.kt
+++ android/app/src/main/java/dev/asbel/himnos/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.himnoapp
+package dev.asbel.himnos
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -12,7 +12,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "himnoApp"
+  override fun getMainComponentName(): String = "himnos"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/dev/asbel/himnos/MainApplication.kt
+++ android/app/src/main/java/dev/asbel/himnos/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.himnoapp
+package dev.asbel.himnos
 
 import android.app.Application
 import com.facebook.react.PackageList
